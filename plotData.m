f16ed76bb99ad5adb4fb57a6d4d1c1334a75e73b
Jason Moore <moorepants@gmail.com>
2010-05-14 09:44:51 -0700

    removed the .m files

--- plotData.m
+++
@@ -1,9 +0,0 @@
-plot(t,data,'*')
-hold on
-[sse, FittedCurve] = model(estimates);
-plot(t, FittedCurve, 'r')
-xlabel('xdata')
-ylabel('f(estimates,xdata)')
-title(['Fitting to function ', func2str(model)]);
-legend('data', ['fit using ', func2str(model)])
-hold off
