d1d889a9465f00f0b9bd6b84c64da93a04b35a57
Jason Moore <moorepants@gmail.com>
2010-05-27 13:50:04 -0700

    successfully replacing quantities in the tex docs

--- MakeParTables.py
+++ MakeParTables.py
@@ -1,5 +1,6 @@
 import pickle
 import re
+import os
 import uncertainties as un
 
 # load in the base data file
@@ -14,9 +15,39 @@ f = open('data/par.p', 'r')
 par = pickle.load(f)
 f.close()
 
+direct = 'ParTables'
 for i, name in enumerate(data['bikes']):
-    fname = ''.join(name.split()) + 'RiderCan.p'
-    file = open(dir + fname)
-    can = pickle.load(file)
-    file.close()
-
+    fname = ''.join(name.split()) 
+    # open the new file
+    f = open(direct + '/ParameterTable.tex', 'r')
+    fn = open(direct + '/' + fname + 'RiderPar.tex', 'w') 
+    for line in f:
+        print line
+        # find all of the matches in the line
+        test = re.findall('\|(\w*.)\|', line)
+        print 'search for u: ',  re.findall('\|(\w*\?)\|', line)
+        print 'search for nom: ', re.findall('\|(\w*)(?!\?)\|', line)
+        # if there are matches
+        if test:
+            print 'Found this!\n', test
+            # go through each match and make a substitution
+            for match in test:
+                print "replace this: ", match
+                if match[-1] == '?':
+                    try:
+                        line = re.sub('\|(\w*\?)\|', '%-6.3f' % par[match[:-1]][i].std_dev(), line,
+                                count=1)
+                        print line
+                    except:
+                        pass
+                else:
+                    try:
+                        line = re.sub('\|(\w*(?!\?))\|',
+                                str(par[match][i].nominal_value), line, count=1)
+                        print line
+                    except:
+                        pass
+        print line
+        fn.write(line)
+    f.close()
+    fn.close()
