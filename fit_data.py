452c492273701f5ed9834dc0bd0acbbcc160e66a
Jason Moore <moorepants@gmail.com>
2010-05-27 20:40:20 -0700

    forgot to use git mv

---
+++ fit_data.py
@@ -0,0 +1,79 @@
+import scipy.io.matlab.mio as mio
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize as op
+import os
+import pickle as p
+from scipy.optimize import approx_fprime
+from uncertainties import num_with_uncert
+from BenchmarkBikeTools import fit_goodness, jac_fitfunc
+
+dirs, subdirs, filenames = list(os.walk('data/pendDat'))[0]
+file = open('data/period.txt', 'w')
+filenames.sort()
+period = {}
+#for name in ['YellowRevForkTorsionalFirst1.mat']:
+#for name in ['YellowFwheelCompoundFirst1.mat']:
+#for name in ['StratosFrameCompoundFirst2.mat']:
+for name in filenames:
+    pendDat = {}
+    mio.loadmat('data/pendDat/' + name, mdict=pendDat)
+    y = pendDat['data'].ravel()
+    time = float(pendDat['duration'][0])
+    x = np.linspace(0, time, num=len(y))
+    # plot the original data
+    plt.plot(x, y, '.')
+    # decaying oscillating exponential function
+    fitfunc = lambda p, t: p[0] + np.exp(-p[3]*p[4]*t)*(p[1]*np.sin(p[4]*np.sqrt(1-p[3]**2)*t) + p[2]*np.cos(p[4]*np.sqrt(1-p[3]**2)*t))
+    # initial guesses
+    p0 = np.array([1.35, -.5, -.75, 0.01, 3.93])
+    # create the error function
+    errfunc = lambda p, t, y: fitfunc(p, t) - y
+    # minimize the error function
+    p1, success = op.leastsq(errfunc, p0[:], args=(x, y))
+    # plot the fitted curve
+    lscurve = fitfunc(p1, x)
+    plt.plot(x, lscurve, 'k')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
+    #plt.show()
+    plt.savefig('data/pendDat/graphs/' + name[:-4] + '.png')
+    plt.close()
+    rsq, SSE, SST, SSR = fit_goodness(y, lscurve)
+    sigma = np.sqrt(SSE/(len(y)-len(p0))) # DoF for non-lin fit may be different
+    # calculate the jacobian
+    L = jac_fitfunc(p1, x)
+    # the Hessian
+    H = np.dot(L.T, L)
+    # the covariance matrix
+    U = sigma**2.*np.linalg.inv(H)
+    # the standard deviations
+    sigp = np.sqrt(U.diagonal())
+    # add a star in the R value is low
+    if rsq <= 0.99:
+        rsq = str(rsq) + '*'
+    else:
+        pass
+    # frequency and period
+    wo = num_with_uncert((p1[4], sigp[4]))
+    zeta = num_with_uncert((p1[3], sigp[3]))
+    wd = (1. - zeta**2.)**(1./2.)*wo
+    f = wd/2./np.pi
+    T = 1./f
+    # include the notes for the experiment
+    note = pendDat['notes']
+    line = name + ',' + str(T) + ',' + str(rsq) + ',' + str(sigma) + ',' + str(note) + '\n'
+    file.write(line)
+    print line
+    # if the filename is already in the period dictionary...
+    if name[:-5] in period.keys():
+        # append the period to the list
+        period[name[:-5]].append(T)
+    # else if the filename isn't in the period dictionary...
+    else:
+        # start a new list
+        period[name[:-5]] = [T]
+file.close()
+file = open('data/period.p', 'w')
+p.dump(period, file)
+file.close()
---
+++ tor_com.py
@@ -0,0 +1,52 @@
+import pickle as p
+import numpy as np
+import re
+
+f = open('data/period.p', 'r')
+period = p.load(f)
+f.close()
+def space_out_camel_case(s):
+        """Adds spaces to a camel case string.  Failure to space out string
+        returns the original string.
+        >>> space_out_camel_case('DMLSServicesOtherBSTextLLC')
+        'DMLS Services Other BS Text LLC'
+        """
+        return re.sub('((?=[A-Z][a-z])|(?<=[a-z])(?=[A-Z]))', ' ', s)
+
+tor = np.zeros((12, 8), dtype='object')
+com = np.zeros((4, 8), dtype='object')
+# list of bike names (only first letter is capatilized)
+bN = ['Browser', 'Browserins', 'Crescendo', 'Fisher', 'Pista', 'Stratos',
+        'Yellow', 'Yellowrev']
+# list of the orientation angles
+fst = ['First', 'Second', 'Third']
+# list of the bicycle parts
+fffr = ['Frame', 'Fork', 'Fwheel', 'Rwheel']
+# list of type of pendulums
+tc = ['Torsional', 'Compound']
+# average the periods
+for k, v in period.items():
+    # substitute names so the camel case function works
+    km = re.sub('BrowserIns', 'Browserins', k)
+    km = re.sub('YellowRev', 'Yellowrev', km)
+    desc = space_out_camel_case(km).split()
+    print desc
+    if desc[0] == 'Rod':
+        rodPeriod = np.mean(v)
+    else:
+        # if torsional, put it in the torsional matrix
+        if desc[2] == tc[0]:
+            r = fffr.index(desc[1])*3 + fst.index(desc[3])
+            c = bN.index(desc[0])
+            tor[r, c] = np.mean(v)
+            print 'Added torsional to [', r, ',', c, ']'
+        # if compound, put in in the compound matrix
+        elif desc[2] == tc[1]:
+            com[fffr.index(desc[1]), bN.index(desc[0])] = np.mean(v)
+avgPer = {}
+avgPer['tor'] = tor
+avgPer['com'] = com
+avgPer['rodPer'] = rodPeriod
+f = open('data/avgPer.p', 'w')
+p.dump(avgPer, f)
+f.close()
