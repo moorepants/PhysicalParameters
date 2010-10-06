8d88baae64d56e30f4cd560111d37b80afe4e13c
Jason Moore <moorepants@gmail.com>
2010-10-05 18:06:04 -0700

    stopping point, starting to add in the rest of the parameter calcs

--- BicycleParameters.py
+++ BicycleParameters.py
@@ -1,6 +1,7 @@
 import os
 import pickle
-from uncertainties import ufloat
+from uncertainties import ufloat, unumpy
+from numpy import pi, zeros, vstack, dot, mean, array, shape, ones
 
 class bicycle(object):
     # these are the various parameter sets
@@ -26,17 +27,19 @@ class bicycle(object):
 
     def __init__(self, shortname):
         '''
+        Sets the parameters if there any that are already saved.
+
         shortname: string
-            shortname of your bicicleta, one word, first letter is capped
+            shortname of your bicicleta, one word, first letter is capped and
+            should match a directory under data/
         '''
 
         self.shortname = shortname
         self.directory = ('data/' + shortname + '/')
+        self.params = {}
 
         # are there any parameters already listed? grab any parameters and
         # store them
-
-        self.params = {}
         match = False
         for typ in self.ptypes:
             pfile = False
@@ -74,6 +77,9 @@ class bicycle(object):
                 else:
                     pass
 
+        if match == False:
+            print "There are no parameters, try calculate_from_measured"
+
     def save(self, filetype='pickle'):
         '''
         Saves all the parameters to file.
@@ -98,67 +104,94 @@ class bicycle(object):
         elif filetype == 'text':
             print "Doesn't work yet"
 
+    def calculate_from_measured(self):
+        '''
+        Calculates the parameters from measured data.
 
-            ###print 'this did not work'
-###
-            #### load the data file
-            ###f = open('data/udata.p', 'r')
-            ###ddU = pickle.load(f)
-            ###f.close()
-###
-            #### the number of different bikes
-            ###nBk = len(ddU['bikes'])
-###
-            #### make a list of the bikes' names
-            ###bikeNames = ddU['bikes']
-###
-            #### calculate all the benchmark parameters
-            ###par = {}
-###
-            #### calculate the wheel radii
-            ###par['rR'] = ddU['rearWheelDist']/2./pi/ddU['rearWheelRot']
-            ###par['rF'] = ddU['frontWheelDist']/2./pi/ddU['frontWheelRot']
-###
-            #### steer axis tilt in radians
-            ###par['lambda'] = pi/180.*(90. - ddU['headTubeAngle'])
-###
-            #### calculate the front wheel trail
-            ###forkOffset = ddU['forkOffset']
-            ###par['c'] = (par['rF']*unumpy.sin(par['lambda'])
-                          ###- forkOffset)/unumpy.cos(par['lambda'])
-###
-            #### wheelbase
-            ###par['w'] = ddU['wheelbase']
-###
-            #### calculate the dees
-            ###par['d1'] = unumpy.cos(par['lambda'])*(par['c']+par['w']-par['rR']*unumpy.tan(par['lambda']))
-            ###par['d3'] = -unumpy.cos(par['lambda'])*(par['c']-par['rF']*unumpy.tan(par['lambda']))
-###
-            #### calculate the frame rotation angle
-            #### alpha is the angle between the negative z pendulum (horizontal) and the
-            #### positive (up) steer axis, rotation about positive y
-            ###alphaFrame = ddU['frameAngle']
-            #### beta is the angle between the x bike frame and the x pendulum frame, rotation
-            #### about positive y
-            ###betaFrame = par['lambda'] - alphaFrame*pi/180
-###
-            #### calculate the slope of the CoM line
-            ###frameM = -unumpy.tan(betaFrame)
-###
-            #### calculate the z-intercept of the CoM line
-            #### frameMassDist is positive according to the pendulum ref frame
-            ###frameMassDist = ddU['frameMassDist']
-            ###cb = unumpy.cos(betaFrame)
-            ###frameB = -frameMassDist/cb - par['rR']
-###
-            #### calculate the fork rotation angle
-            ###betaFork = par['lambda'] - ddU['forkAngle']*np.pi/180.
-###
-            #### calculate the slope of the fork CoM line
-            ###forkM = -unumpy.tan(betaFork)
-###
-            #### calculate the z-intercept of the CoM line
-            ###forkMassDist = ddU['forkMassDist']
-            ###cb = unumpy.cos(betaFork)
-            ###tb = unumpy.tan(betaFork)
-            ###forkB = - par['rF'] - forkMassDist/cb + par['w']*tb
+        '''
+
+        # load the measured data file
+        f = open(self.directory + self.shortname + 'Measured.p', 'r')
+        ddU = pickle.load(f)
+        f.close()
+        print ddU
+
+        # calculate all the benchmark parameters
+        par = {}
+
+        # calculate the wheel radii
+        par['rR'] = ddU['rearWheelDist']/2./pi/ddU['rearWheelRot']
+        par['rF'] = ddU['frontWheelDist']/2./pi/ddU['frontWheelRot']
+
+        # steer axis tilt in radians
+        par['lambda'] = pi/180.*(90. - ddU['headTubeAngle'])
+
+        # calculate the front wheel trail
+        forkOffset = ddU['forkOffset']
+        par['c'] = (par['rF']*unumpy.sin(par['lambda'])
+                      - forkOffset)/unumpy.cos(par['lambda'])
+
+        # wheelbase
+        par['w'] = ddU['wheelbase']
+
+        # calculate the frame rotation angle
+        # alpha is the angle between the negative z pendulum (horizontal) and the
+        # positive (up) steer axis, rotation about positive y
+        alphaFrame = ddU['frameAngle']
+        # beta is the angle between the x bike frame and the x pendulum frame, rotation
+        # about positive y
+        betaFrame = par['lambda'] - alphaFrame*pi/180
+
+        # calculate the slope of the CoM line
+        frameM = -unumpy.tan(betaFrame)
+
+        # calculate the z-intercept of the CoM line
+        # frameMassDist is positive according to the pendulum ref frame
+        frameMassDist = ddU['frameMassDist']
+        cb = unumpy.cos(betaFrame)
+        frameB = -frameMassDist/cb - par['rR']
+
+        # calculate the fork rotation angle
+        betaFork = par['lambda'] - ddU['forkAngle']*pi/180.
+
+        # calculate the slope of the fork CoM line
+        forkM = -unumpy.tan(betaFork)
+
+        # calculate the z-intercept of the CoM line
+        forkMassDist = ddU['forkMassDist']
+        cb = unumpy.cos(betaFork)
+        tb = unumpy.tan(betaFork)
+        forkB = - par['rF'] - forkMassDist/cb + par['w']*tb
+
+        # intialize the matrices for the center of mass locations
+        frameCoM = zeros((2, shape(frameM)[1]), dtype='object')
+        forkCoM = zeros((2, shape(forkM)[1]), dtype='object')
+
+        # for each of the bikes...
+        for i in range(shape(frameM)[1]):
+            comb = array([[0, 1], [0, 2], [1, 2]])
+            # calculate the frame center of mass position
+            # initialize the matrix to store the line intersections
+            lineX = zeros((3, 2), dtype='object')
+            # for each line intersection...
+            for j, row in enumerate(comb):
+                a = unumpy.matrix(vstack([-frameM[row, i], ones((2))]).T)
+                b = frameB[row, i]
+                lineX[j] = dot(a.I, b)
+            frameCoM[:, i] = mean(lineX, axis=0)
+            # calculate the fork center of mass position
+            # reinitialize the matrix to store the line intersections
+            lineX = zeros((3, 2), dtype='object')
+            # for each line intersection...
+            for j, row in enumerate(comb):
+                a = unumpy.matrix(vstack([-forkM[row, i], ones((2))]).T)
+                b = forkB[row, i]
+                lineX[j] = dot(a.I, b)
+            forkCoM[:, i] = mean(lineX, axis=0)
+
+        par['xB'] = frameCoM[0, :]
+        par['zB'] = frameCoM[1, :]
+        par['xH'] = forkCoM[0, :]
+        par['zH'] = forkCoM[1, :]
+
+        self.params['Benchmark'] = par
